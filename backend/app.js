c46735dba22e49f8d520972c51e32b7934cbb808
Vedant Sahu <vedant@pop-os.localdomain>
2024-01-04 20:24:29 +0530

    first

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+
+    origin: [process.env.FRONTEND_URL],
+    credentials : true
+
+}));
+
+app.use(cookieParser());
+
+app.use('./ping',function (req,res){
+    res.send('/pong')
+})
+
+// routes of 3 modules
+
+app.all('*',(res,req)=>{
+    res.status(404).send('OPPS! 404 NOT FOUND');
+})
+module.exports = app;
