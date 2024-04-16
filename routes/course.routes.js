e052cd04c5b252b49ff20a8211dba4262520c0dd
vedant <vedant@ssipmt.com>
2024-04-16 12:29:40 +0530

    add on

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,9 +1,10 @@
 import {Router} from 'express';
 import { getAllCourses ,getLecturesByCourseID} from '../controllers/course.controller.js'
+import { isLoggedIn } from '../middlewares/auth.middleware.js';
 
 const Courserouter = new Router();
 
 Courserouter.get('/',getAllCourses);
-Courserouter.get('/:id',getLecturesByCourseID);
+Courserouter.get('/:id',isLoggedIn,getLecturesByCourseID);
 
 export default Courserouter;
