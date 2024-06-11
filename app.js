2b2e447aaf34f4d28f29be95e741057fd1267bff
vedant <vedant@ssipmt.com>
2024-06-11 22:42:34 +0530

    ready deploy

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
 import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
+import cors from 'cors';
 import morgan from 'morgan';
-import userRoutes from './routes/user.routes.js'
-import errorMiddleware from './middlewares/error.middleware.js'
-import Courserouter from './routes/course.routes.js';
-import paymentRouter from './routes/payment.routes.js';
-import miscRoutes from './routes/miscellaneous.routes.js'
+import cookieParser from 'cookie-parser';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 config();
 
 const app = express();
 
-app.use(morgan('dev'))
-
+// Middlewares
+// Built-In
 app.use(express.json());
-
-app.use(cors({
-    origin: 'https://mentorwaveonline.netlify.app/',
-    credentials: true
-}));
-
+app.use(express.urlencoded({ extended: true }));
+// Third-Party
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Allow all methods
+  })
+);
+app.use(morgan('dev'));
 app.use(cookieParser());
 
-app.use('/ping', (req,res)=>{
-    res.send('/pong')
-})
-//module routes 
-app.use('/api/v1/user',userRoutes)
+// Server Status Check Route
+app.get('/ping', (_req, res) => {
+  res.send('Pong');
+});
 
-app.use('/api/v1/courses',Courserouter)
+// Import all routes
+import userRoutes from './routes/user.routes.js';
+import courseRoutes from './routes/course.routes.js';
+import paymentRoutes from './routes/payment.routes.js';
+import miscRoutes from './routes/miscellaneous.routes.js';
 
-app.use('/api/v1/payments',paymentRouter)
-
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/courses', courseRoutes);
+app.use('/api/v1/payments', paymentRoutes);
 app.use('/api/v1', miscRoutes);
 
-app.all('*',(req,res)=>{
-    res.status(404).send('OOPs 404 erorr Page not Found');
-})
+// Default catch all route - 404
+app.all('*', (_req, res) => {
+  res.status(404).send('OOPS!!! 404 Page Not Found');
+});
 
-app.use(errorMiddleware)
+// Custom error handling middleware
+app.use(errorMiddleware);
 
 export default app;
